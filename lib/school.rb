cc35cc2cf4ce3e03dd37863205e1d95ff4aaed16
Adao <sansonov@gmail.com>
2020-04-21 18:34:03 +0100

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,35 @@
-# code here!
+require 'pry'
+class School
+
+    attr_reader :name, :roster
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(student_name, grade)
+        @student_name = student_name
+        @grade = grade
+
+        if !@roster[@grade]
+            @roster[@grade] = [] 
+        end
+            @roster[@grade] << @student_name
+    end
+    
+    def grade(grade)
+        @grade = grade
+        @roster.find {|key, value| key == 10}.slice(1)
+    end
+
+    def sort
+        new_roster = {}
+        @roster.each do |k, v| 
+            a = @roster[k] = v.sort
+            new_roster[k] = a
+             #binding.pry
+        end
+        new_roster
+    end
+
+end
